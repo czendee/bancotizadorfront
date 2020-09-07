dcac49d5b07cb8e8f2b7670cbab83fbd76f95968
Don Carlos Zendejas <czende@hotmail.com>
2020-09-07 12:27:55 -0500

    Update Filtro.jsx

--- src/components/reportes/Filtro.jsx
+++ src/components/reportes/Filtro.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import calendario from '../../assets/img/cal.jpg';
 const FiltroReportes= () => {
 
+   /*
    var traeCotizaciones = () => {
         
         const data = { numero : "122", name: "primero"};
@@ -14,7 +15,7 @@ const FiltroReportes= () => {
                 'Content-Type':'application/json'
             }),
         };
-        
+     */   
 /*
         if (!pattern.test(data.email)){
             this.setState({ message : 'El correo electronicó no es válido.' });  
@@ -26,7 +27,7 @@ const FiltroReportes= () => {
             return
         }
 */
-
+/*
         fetch('http://peaceful-retreat-91246.herokuapp.com/banwireapi/cotizaciones', requestInfo)
         .then(response =>{
             if(response.ok){
@@ -39,7 +40,7 @@ const FiltroReportes= () => {
             this.setState({ message: e.message });   
         });
     }
-    
+  */  
 
   return (
       <Fragment>
@@ -122,7 +123,7 @@ const FiltroReportes= () => {
                             </div>
                             <div className="d50 left_">
                                 <div className="d97 right_">
-                                    <input type="submit" className="right_ mar_t_3 bot_value" value="FILTRAR AHORA" onClick={this.traeCotizaciones} />
+                                    <input type="submit" className="right_ mar_t_3 bot_value" value="FILTRAR AHORA carlos"  />
                                 </div>
                             </div>
                         </div>
